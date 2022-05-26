cd5f21ba844609de948696a9efb3b3d4075a378d
JuhaniRusi <juhanirusi@yahoo.com>
2022-05-26 12:51:48 +0300

    Another commit.

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # OUR APPS
+    'core',
 ]
 
 MIDDLEWARE = [
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,40 @@
+"""
+Test custom Django management commands.
+"""
+
+from lib2to3.pytree import Base
+from unittest.mock import patch
+
+# call_command is a helper function that allows us to simulate or to
+# actually call the command by the name, e.g. call the command we're testing.
+from django.core.management import call_command
+# Another Operational error that might be raised during the process
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+# OperationalError is one of the possibilities that might occur when we try
+# to connect to the database before the database is ready.
+from psycopg2 import OperationalError as Psycopg2Error
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Django command to wait for the database"""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError"""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
