b47733f781e3ef4e3a625ca571eed727c9248f42
JuhaniRusi <juhanirusi@yahoo.com>
2022-05-26 19:47:14 +0300

    another commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # OUR APPS
     'core',
 ]
@@ -130,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,45 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    # extra_fields (extra keyword arguments) -> This is useful when adding
+    # additional fields to the user model + you don't need to add the fields
+    # to the 'create_user' in any other way.
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address!')
+        # DON'T MISSPELL THIS, IT NEEDS TO BE CALLED "user"
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+# PermissionsMixin --> Functionality for the permissions & fields
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
