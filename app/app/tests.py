81109bde327dc75daa43cd20b30b3b963e6c07e2
JuhaniRusi <juhanirusi@yahoo.com>
2022-05-26 13:00:12 +0300

    new try

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,7 +7,8 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """Test the calc module"""    
+    """Test the calc module"""
+
     def test_add_numbers(self):
         """Test adding numbers together."""
         result = calc.add(5, 6)
